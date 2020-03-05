b9496880161044c47e843608bb6b0592b6bfeda6
Yousuf Asfari <43550346+kodekoala@users.noreply.github.com>
2020-03-04 23:56:34 -0500

    Added list_mbox and send_msg functionality

--- proj1proto/mailbox_syscalls.c
+++ proj1proto/mailbox_syscalls.c
@@ -16,12 +16,6 @@ typedef struct msgNode {
 }
 msgNode_t;
 
-typedef struct AccessControlNode {
-  pid_t pid;
-  struct list_head list_node;
-}
-AccessControlNode_t;
-
 typedef struct mbox {
   unsigned long boxId;
   int encryption;
@@ -72,10 +66,9 @@ long create_mbox_421(unsigned long id, int crypt_alg) {
     }
   }
 
-  //if it does not exist, create new mailbox
+  //if the mailbox doesn't exist, create new mailbox
   mbox_t * new_mbox = (mbox_t * ) malloc(sizeof(mbox_t));
   INIT_LIST_HEAD( & new_mbox -> msgs);
-  INIT_LIST_HEAD( & new_mbox -> ACL);
   new_mbox -> boxId = id;
   new_mbox -> encryption = crypt_alg;
   //Insert the new list_node for the created mailbox before the head
@@ -92,7 +85,7 @@ long create_mbox_421(unsigned long id, int crypt_alg) {
    an appropriate error and not remove the mailbox.
  */
 //SYSCALL_DEFINE1(remove_mbox_421, unsigned long, id) {}
-long remove_mbox(unsigned long id) {
+long remove_mbox_421(unsigned long id) {
 
   if (geteuid() != 0) {
     // Tell user to run app as root, then exit.
@@ -110,32 +103,15 @@ long remove_mbox(unsigned long id) {
 
       if (list_empty( &box -> msgs)) { //true if empty
         printf("Empty messages in this mailbox\n");
-
-        if (!list_empty( &box -> ACL)) { //true if ACL not empty
-          printf("ACL not empty\n");
-          //delete ACL
-          AccessControlNode_t * the_pid;
-          AccessControlNode_t * temp_pid;
-
-          //Get the AccessControl struct that corresponds to the list_node 
-          //that is currently pointed to by the the_pid pointer 
-          list_for_each_entry_safe(the_pid, temp_pid, &box -> ACL, list_node) {
-            printf("pid %i removed\n", the_pid -> pid);
-            list_del(&the_pid -> list_node);
-            //kfree(the_pid);
-            free(the_pid);
-          }
-          printf("Deleted ACL for mbox %lu\n", box -> boxId);
-        }
-
-        printf("Deleted the mailbox %lu\n", box -> boxId);
+        printf("Deleted the mailbox with ID: %lu\n", box -> boxId);
         list_del(currBox);
-        //kfree(p);
+        //kfree(currBox);
         free(box);
+        mailboxCount--;
         return 0;
       } 
       else {
-        printf("mailbox %lu is not empty\n", id);
+        printf("The mailbox %lu is not empty\n", id);
         return ENOTEMPTY;
       }
     }
@@ -145,8 +121,129 @@ long remove_mbox(unsigned long id) {
 }
 
 
+/* 
+Returns a list of up to k mailbox IDs in the user-space variable mbxes.
+It returns the number of IDs written successfully to mbxes on success 
+and an appropriate error code on failure.
+*/
+//SYSCALL_DEFINE5(list_mbox_421, unsigned long __user *, mbxes, long, k) {
+long list_mbox_421(unsigned long * mbxes, long k) {
+
+  //check if passed in pointer is valid
+  if (mbxes == NULL || k < 0)
+    return EFAULT;
+  
+  struct list_head *pos;
+  unsigned int writtenIDs = 0; //num of written IDs
+  
+  //User is requesting for too many mboxes
+  if (k > mailboxCount)
+    return EFAULT;
+    
+  list_for_each(pos, &mailBoxes) {
+    mbox_t* theBox = NULL;
+    theBox = list_entry(pos, mbox_t, list_node);
+    if (writtenIDs < k) {
+      printf("Mailbox %lu\n", theBox->boxId);
+      mbxes[writtenIDs] = theBox->boxId;
+      writtenIDs++;
+    }
+    
+  }
+  return writtenIDs;
+}
+
+/* returns the number of existing mailboxes.
+ */
+//SYSCALL_DEFINE4(count_mbox_421, void) {
+long int count_mbox_421(void) {
+  return mailboxCount;
+}
+
+/* 
+Encrypts the message msg (using the correct algorithm), adding it to the already existing
+mailbox identified. Returns the number of bytes stored (which shall be equal to the message 
+length n) on success, and an appropriate error code on failure. Messages with negative lengths 
+shall be rejected as invalid and cause an appropriate error to be returned, however messages 
+with a length of zero shall be accepted as valid.
+ */
+//SYSCALL_DEFINE6(send_msg_421, unsigned long, id, unsigned char __user *, msg, long, n, uint32_t __user *, key) {}
+long send_msg_421(unsigned long id, unsigned char * msg, long n, uint32_t * key) {
+
+  if (msg == NULL) //check passed in pointer
+    return EFAULT;
+  if (n < 0) //check msg length n for invalid size
+    return EIO;
+
+  struct list_head * currBox;
+  list_for_each(currBox, &mailBoxes) { //loop mailboxes
+    mbox_t * box = NULL;
+    box = list_entry(currBox, mbox_t, list_node);
+
+    //search for mailbox id
+    if (box -> boxId == id) {
+
+      if (box -> encryption == 0) {
+        //XOR Cipher
+      }
+      else{
+        //XTEA Cipher
+      }
+
+      long count = 0;
+
+      //add to mailbox
+      msgNode_t * msgNode = (msgNode_t * ) malloc(sizeof(msgNode_t));
+      msgNode -> msg = (unsigned char * ) malloc(n * sizeof(unsigned char));
+
+      //copy msg
+      for (int i = 0; i < n; i++) {
+        msgNode -> msg[i] = msg[i];
+        count++;
+      }
+
+      msgNode -> msgLen = n;
+
+      //      msgNode->msg = msg;
+      list_add_tail( &msgNode - > list_node, &box - > msgs);
+      return count; //number of bytes stored on success
+      //error code on failure
+    }
+  }
+  printf("Couldn't find mailbox with %lu ID\n", id);
+  return ENOENT;
+}
+
+
 int main(void) {
+  long k = 5;
+  unsigned long * mbxes = (unsigned long * ) malloc(sizeof(unsigned long) * k);;
   create_mbox_421(50, 0);
-  remove_mbox(50);
+  create_mbox_421(51, 0);
+  create_mbox_421(52, 0);
+  create_mbox_421(53, 0);
+  create_mbox_421(54, 0);
+
+  printf("Number of mailboxes: %lu\n", count_mbox_421());   // unsigned long
+
+
+  list_mbox_421(mbxes, k);
+  remove_mbox_421(50);
+  remove_mbox_421(51);
+  remove_mbox_421(52);
+  remove_mbox_421(53);
+  remove_mbox_421(54);
+
+  printf("Number of mailboxes: %lu\n", count_mbox_421());   // unsigned long
+
+
+  long i = 0;
+  while (i < k){
+    printf("ID: %lu\n", mbxes[i]);   // unsigned long
+    i++;
+  }
+
+  free(mbxes);
+
   return 0;
 }
